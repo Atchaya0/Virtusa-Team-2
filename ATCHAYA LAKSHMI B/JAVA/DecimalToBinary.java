485879562cac471813fa45dcf795e90251ca8fc9
AtchayalakshmiB <143797818+AtchayalakshmiB@users.noreply.github.com>
2026-09-22 10:24:31 +0530

    Update DecimalToBinary.java

--- ATCHAYA LAKSHMI B/JAVA/DecimalToBinary.java
+++ ATCHAYA LAKSHMI B/JAVA/DecimalToBinary.java
@@ -1,40 +1,19 @@
 import java.util.Scanner;
-public class DecimalToBinary {
-    public static String decimalToBinary(long decimal) {
-        if (decimal == 0) {
-            return "0";
-        }
 
-        boolean isNegative = decimal < 0;
-        long num = Math.abs(decimal);
-        StringBuilder binary = new StringBuilder();
-
-        while (num > 0) {
-            long remainder = num % 2;
-            binary.append(remainder);
-            num /= 2;
-        }
-        String result = binary.reverse().toString();
-        return isNegative ? "-" + result : result;
-    }
-
+class DecimalToBinary {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a decimal number: ");
-        if (scanner.hasNextLong()) {
-            long decimal = scanner.nextLong();
-            String binaryCustom = decimalToBinary(decimal);
-            String binaryBuiltIn = Long.toBinaryString(decimal);
+        int n = sc.nextInt();
 
-            System.out.println("\n--- Conversion Results ---");
-            System.out.println("Decimal Input       : " + decimal);
-            System.out.println("Binary (Algorithm)  : " + binaryCustom);
-            System.out.println("Binary (Built-in)   : " + binaryBuiltIn);
-        } else {
-            System.out.println("Invalid input. Please enter an integer.");
+        String binary = "";
+
+        while (n > 0) {
+            binary = (n % 2) + binary;
+            n = n / 2;
         }
 
-        scanner.close();
+        System.out.println("Binary = " + binary);
     }
 }
