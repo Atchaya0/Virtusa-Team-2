4e7c9326f1c752c779fde6d1e1da2e02c8072eff
AtchayalakshmiB <143797818+AtchayalakshmiB@users.noreply.github.com>
2026-09-22 15:35:30 +0530

    Update secondSmallest.js

--- ATCHAYA LAKSHMI B/JAVASCRIPT/secondSmallest.js
+++ ATCHAYA LAKSHMI B/JAVASCRIPT/secondSmallest.js
@@ -1,17 +1,17 @@
 let input = prompt("Enter array elements separated by spaces:");
 let arr = input.split(" ").map(Number);
 
-let largest = -Infinity;
-let secondLargest = -Infinity;
+let smallest = Infinity;
+let secondSmallest = Infinity;
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > largest) {
-        secondLargest = largest;
-        largest = arr[i];
+    if (arr[i] < smallest) {
+        secondSmallest = smallest;
+        smallest = arr[i];
     } 
-    else if (arr[i] > secondLargest && arr[i] != largest) {
-        secondLargest = arr[i];
+    else if (arr[i] < secondSmallest && arr[i] != smallest) {
+        secondSmallest = arr[i];
     }
 }
 
-console.log("Second largest =", secondLargest);
+console.log("Second smallest =", secondSmallest);
